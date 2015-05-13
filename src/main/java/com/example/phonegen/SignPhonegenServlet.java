137487a95800fc092758920aeec8f97e7607dca2
Roman Bondarenko <bondarenko.roman@gmail.com>
2015-05-13 01:27:28 -0400

    Initial version of the PhoneGen source code

---
+++ src/main/java/com/example/phonegen/SignPhonegenServlet.java
@@ -0,0 +1,44 @@
+package com.example.phonegen;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.objectify.ObjectifyService;
+
+public class SignPhonegenServlet extends HttpServlet {
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    Greeting greeting;
+
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+
+    String userName = req.getParameter("userName");
+    String content = req.getParameter("content");
+
+    if (user != null) {
+      greeting = new Greeting(userName, content, user.getUserId(), user.getEmail());
+    } else {
+      greeting = new Greeting(userName, content);
+    }
+
+    // Use Objectify to save the greeting and now() is used to make the call synchronously as we
+    // will immediately get a new page using redirect and we want the data to be present.
+    ObjectifyService.ofy().save().entity(greeting).now();
+
+    resp.sendRedirect("/phonegen.jsp?userName=" + userName);
+  }
+}
